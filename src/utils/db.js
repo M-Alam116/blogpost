0a4dc50cb47ca5cdd481f66ca531babf12813185
Muhammad Alam <malam53345@gmail.com>
2023-10-04 23:02:41 +0500

    bugs

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB);
+    await mongoose.connect(
+      "mongodb+srv://M_Alam:NwZCrwKFNkSE06z6@cluster0.dg8uvws.mongodb.net/blogpost?retryWrites=true&w=majority"
+    );
   } catch (error) {
     throw new Error("Connection failed!");
   }
